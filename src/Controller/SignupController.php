22133ef84381083e1edeeb23d91df337cbf925cc
Amandine Sismondi <am.sismondi@gmail.com>
2022-12-22 22:08:50 +0100

    database update implemented and password hashed

--- src/Controller/SignupController.php
+++ src/Controller/SignupController.php
@@ -4,19 +4,38 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\SignupType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SignupController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/signup', name: 'signup')]
-    public function index(): Response
+    public function index(Request $req, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(SignupType::class, $user);
-
+        $form = $form->handleRequest($req);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $user = $form->getData();
+            $password = $passwordHasher->hashPassword($user, $user->getPassword());
+            dd($password); //debbug
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
 
 
 
--- src/Form/SignupType.php
+++ src/Form/SignupType.php
@@ -4,6 +4,13 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+# Added path to TextType manually
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +19,38 @@ class SignupType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr'=> [
+                    'placeholder' => 'Entrez votre prénom'
+                ]
+            ])
+            ->add('Lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Entrez votre email'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques',
+                'label' => 'Mot de passe',
+                'required' => true,
+                'first_options' =>['label'=> 'Mot de passe'],
+                'second_options' =>['label'=> 'Confirmation de mot de passe'],
+                'attr' => [
+                    'placeholder' => 'Entrez votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
         ;
     }
 
