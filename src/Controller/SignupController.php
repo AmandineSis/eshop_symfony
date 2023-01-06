7aeba0887690e5f5763678dd21891765a1047026
Amandine Sismondi <am.sismondi@gmail.com>
2023-01-06 11:35:25 +0100

    form added

--- src/Controller/SignupController.php
+++ src/Controller/SignupController.php
@@ -4,44 +4,45 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\SignupType;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SignupController extends AbstractController
 {
-    private $entityManager;
+    private $entityManager; //convention de nommage pour doctrine
 
     public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->entityManager = $entityManager;
+        $this->entityManager = $entityManager; //mets l'entityManager que l'on vient d'instancier dans la variable privée entityManager
     }
 
-    #[Route('/signup', name: 'signup')]
-    public function index(Request $req, UserPasswordHasherInterface $passwordHasher): Response
+    #[Route('/signup', name: 'app_signup')]
+
+    public function index(Request $req, UserPasswordHasherInterface $encoder): Response
     {
         $user = new User();
-        $form = $this->createForm(SignupType::class, $user);
-        $form = $form->handleRequest($req);
+        $form =  $this->createForm(SignupType::class, $user); //création du formulaire
         
-        if ($form->isSubmitted() && $form->isValid()) { 
+        $form->handleRequest($req);
+
+        if($form->isSubmitted() && $form->isValid()){
             $user = $form->getData();
-            $password = $passwordHasher->hashPassword($user, $user->getPassword());
-            dd($password); //debbug
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
+
+            $password = $encoder->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $this->entityManager->persist($user);//figer les données
+            $this->entityManager->flush();//envoyer les données vers bdd
         }
 
 
-
         return $this->render('signup/index.html.twig', [
-            'controller_name' => 'SignupController',
-            'form' => $form,
+            'myForm' => $form->createView(), //crée la vue du formulaire
+            
         ]);
     }
 }
--- src/Form/SignupType.php
+++ src/Form/SignupType.php
@@ -4,53 +4,57 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-# Added path to TextType manually
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use function PHPSTORM_META\type;
 
 class SignupType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', TextType::class, [
-                'label' => 'Votre prénom',
-                'attr'=> [
-                    'placeholder' => 'Entrez votre prénom'
+            
+            ->add('firstname', TextType::class,[
+                'label'=>'Prénom',
+                'attr'=>[
+                    'placeholder'=>'Entrez votre prénom'
                 ]
-            ])
-            ->add('Lastname', TextType::class, [
-                'label' => 'Votre nom',
-                'attr' => [
-                    'placeholder' => 'Entrez votre nom'
+            ]) //input
+
+            ->add('lastname', TextType::class,[
+                'label'=>'Nom',
+                'attr'=>[
+                    'placeholder'=>'Entrez votre nom'
                 ]
-            ])
-            ->add('email', EmailType::class, [
-                'label' => 'Email',
-                'attr' => [
-                    'placeholder' => 'Entrez votre email'
+            ]) //input
+
+            ->add('email', EmailType::class,[
+                'label'=>'Email',
+                'attr'=>[
+                    'placeholder'=>'Entrez votre adresse email'
                 ]
-            ])
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'Les mots de passe doivent être identiques',
-                'label' => 'Mot de passe',
-                'required' => true,
-                'first_options' =>['label'=> 'Mot de passe'],
-                'second_options' =>['label'=> 'Confirmation de mot de passe'],
-                'attr' => [
-                    'placeholder' => 'Entrez votre mot de passe'
-                ]
-            ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Envoyer'
-            ])
+            ]) //input
+
+            ->add('password', RepeatedType::class,[
+                'type'=> PasswordType::class,
+                'invalid_message'=>'les mots de passe doivent être identiques',
+                'label'=>'Mot de passe',
+                'required'=>true,
+                'first_options'=>['label'=> 'Mot de passe'],
+                'second_options'=>['label'=>'Confirmez le mot de passe']
+            ]) //input
+
+            ->add('submit', SubmitType::class,[
+                'label'=>"S'inscrire",
+                
+            ]) //input
+            
         ;
     }
 
@@ -61,3 +65,4 @@ class SignupType extends AbstractType
         ]);
     }
 }
+
