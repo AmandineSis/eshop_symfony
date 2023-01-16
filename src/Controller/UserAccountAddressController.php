5eb5b17e9628085511f13075acaed516702c0d56
Amandine Sismondi <am.sismondi@gmail.com>
2023-01-16 22:12:28 +0100

    address add function data managed

--- src/Controller/UserAccountAddressController.php
+++ src/Controller/UserAccountAddressController.php
@@ -4,12 +4,22 @@ namespace App\Controller;
 
 use App\Entity\Address;
 use App\Form\AddressType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserAccountAddressController extends AbstractController
+
 {
+    private $entityManager; //convention de nommage pour doctrine
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager; //mets l'entityManager que l'on vient d'instancier dans la variable privée entityManager
+    }
+
     #[Route('/user/account/address', name: 'account_address')]
     public function index(): Response
     {
@@ -17,10 +27,23 @@ class UserAccountAddressController extends AbstractController
     }
 
     #[Route('/user/account/add_address', name: 'account_add_address')]
-    public function add(): Response
+    public function add(Request $req): Response
     {
         $address = new Address();
         $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $address->setUser($this->getUser()); //ajoute aux données de l'adresse le nom de l'utilisateur
+
+            $this->entityManager->persist($address); //figer les données
+            $this->entityManager->flush(); //envoyer les données vers bdd
+            return $this->redirectToRoute('account_address');
+        }
+
+
+
         return $this->render('user_account/add_address.html.twig', [
             'address_form' => $form->createView()
         ]);
--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -3,29 +3,98 @@
 namespace App\Form;
 
 use App\Entity\Address;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class AddressType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('company')
-            ->add('address')
-            ->add('postcode')
-            ->add('city')
-            ->add('country')
-            ->add('phone')
-            ->add('submit', SubmitType::class,[
-                'label'=> 'Ajouter mon adresse'
+            ->add('name', TextType::class, [
+                'label' => 'Quel nom souhaitez-vous donner à cette adresse ?',
+                'attr' => [
+                    'placeholder' => 'Entrez le nom de votre adresse'
+                ]
             ])
-        ;
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30,
+                    'minMessage' => 'Votre prénom doit contenir au moins 2 caractères',
+                    'maxMessage' => 'Votre prénom ne doit pas contenir plus de 30 caractères'
+                ]),
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30,
+                    'minMessage' => 'Votre nom doit contenir au moins 2 caractères',
+                    'maxMessage' => 'Votre nom ne doit pas contenir plus de 30 caractères'
+                ]),
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom'
+                ]
+            ])
+            ->add('company', TextType::class, [
+                'label' => 'Compagnie',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => '(Facultatif)Entrez le nom de votre socité'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse'
+                ]
+            ])
+            ->add('postcode', TextType::class, [
+                'label' => 'Code postal',
+                'attr' => [
+                    'placeholder' => 'Entrez votre code postal'
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Entrez le nom de votre ville'
+                ]
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'Pays',
+                'label_attr' => [
+                    'class' => 'block'
+                ],
+                'attr' => [
+                    'placeholder' => 'Votre pays',
+                    'class' => 'form-select-sm form-control'
+                ],
+
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Téléphone',
+                'attr' => [
+                    'placeholder' => 'Entrez votre numéro de téléphone'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Ajouter mon adresse',
+                'attr' => [
+                    'class' => "btn-block btn-info"
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
