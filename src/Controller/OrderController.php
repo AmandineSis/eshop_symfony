c96818c3e6bd409502625af9059d812b91402928
Amandine Sismondi <am.sismondi@gmail.com>
2023-01-17 14:26:33 +0100

    form handleRequest added to orderController.php

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,14 +4,26 @@ namespace App\Controller;
 
 use App\Classes\Cart;
 use App\Form\OrderType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+
+    private $entityManager; //convention de nommage pour doctrine
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager; //mets l'entityManager que l'on vient d'instancier dans la variable privée entityManager
+    }
+
     #[Route('/order', name: 'order')]
-    public function index(Cart $cart): Response
+    public function index(Cart $cart, Request $req): Response
     {
         if (!$this->getUser()->getAddresses()->getValues()) {
             return $this->redirectToRoute('account_add_address');
@@ -21,6 +33,11 @@ class OrderController extends AbstractController
             'user' => $this->getUser() //permet de récup du coté formulaire en option l'utilisateur en cours
         ]);
 
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+        }
 
         return $this->render('order/index.html.twig', [
             'orderForm' => $form->createView(),
